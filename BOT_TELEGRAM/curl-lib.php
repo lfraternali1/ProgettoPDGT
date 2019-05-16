86d6eaf40be89449569a27c325afc36362884bc7
Lorenzo Fraternali <l.fraternali1@campus.uniurb.it>
2019-05-16 02:16:19 +0200

    Aggiornamento per gestione richieste POST PUT PATCH

--- BOT_TELEGRAM/curl-lib.php
+++ BOT_TELEGRAM/curl-lib.php
@@ -1,23 +1,46 @@
 <?php
-   // Semplice libreria per le creazione di richieste HTTP
-   function http_request($url) {
-        $handle = curl_init($url);
-        if($handle == false) {
-            die("Ops, cURL non funziona\n");
-        }
+/********************************************************/
+/* Semplice libreria per le creazione di richieste HTTP */
+/********************************************************/
 
-        curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
-        curl_setopt($handle, CURLOPT_HTTPHEADER, array(
-                    "User-Agent: il mio primo script PHP"));
-        // Esecuzione della richiesta, $response = contenuto della risposta testuale
-        $response = curl_exec($handle);
-
-        $status = curl_getinfo($handle, CURLINFO_HTTP_CODE);
-        if($status != 200) {
-            echo("Richiesta HTTP fallita, status {$status}\n");
-        }
-
-        // Decodifica della risposta JSON
-        return json_decode($response);
+function http_request($url, 
+					  $method, 
+					  $body = null) 
+{
+	// Preparo l'handle del cURL
+	$handle = curl_init($url);
+    if($handle == false) 
+	{
+        echo ("Ops, cURL non funziona\n");
+		return (false);
     }
+	curl_setopt($handle, CURLOPT_URL, $url);
+    curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+	curl_setopt($handle, CURLOPT_USERAGENT, "Bot Telegram TurismoMarche");
+	curl_setopt($handle, CURLOPT_CUSTOMREQUEST, $method);
+	// Gestione richieste che prevedono l'invio di dati
+	if (($method === "POST")  ||
+	    ($method === "PUT")   ||
+	    ($method === "PATCH"))
+	{
+		if ($body)
+		{
+			$json = json_encode($body);
+			curl_setopt($handle, CURLOPT_POSTFIELDS, $json);
+		}
+	}
+
+    // Esecuzione della richiesta
+	// $response = contenuto della risposta testuale
+    $response = curl_exec($handle);
+    $status = curl_getinfo($handle, CURLINFO_HTTP_CODE);
+    if($status != 200)
+	{
+		echo ("Errore! Codice: ".$status);		
+	    return (false);
+    }
+
+    // Decodifica della risposta JSON
+    return json_decode($response);
+}
 ?>
