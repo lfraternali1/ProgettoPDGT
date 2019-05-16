21314116592fc1c97ecc89554b8a3c7d8f855370
Lorenzo Fraternali <40865847+lfraternali1@users.noreply.github.com>
2019-05-16 16:29:52 +0200

    Config. con variabili d'ambiente

---
+++ BOT_TELEGRAM/config.php
@@ -0,0 +1,15 @@
+<?php
+
+/**************************/
+/* File di configurazione */
+/**************************/
+
+// Definizione token per interagire con il bot telegram
+define('TOKEN', getenv('TOKEN'));
+// Definizione URI base per API telegram
+define('TELEGRAM_API', getenv('TELEGRAM_API'));
+// Definizione URI base per API turismoMarche
+define('TURISMO_API', getenv('TURISMO_API'));
+// Definizione URI base per FIREBASE
+define('FIREBASE', getenv('FIREBASE')); 
+?>
